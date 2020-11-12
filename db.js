cb4f0f9c20089325e8d6968f6d8178d3f0e30b35
ashtonsprunger <sprungerashton@gmail.com>
2020-11-12 18:59:17 -0500

    password change

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("Carissues", "fwhdlxgbwstzpm", "ashtonefa", {
-  host: "ec2-54-158-222-248.compute-1.amazonaws.com",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  "Carissues",
+  "fwhdlxgbwstzpm",
+  "01fbed73cd0868345c362d5a3d404faecb283eae001411b596ba3f9f254e672f",
+  {
+    host: "ec2-54-158-222-248.compute-1.amazonaws.com",
+    dialect: "postgres",
+  }
+);
 
 let User = sequelize.import("./models/user");
 let Issue = sequelize.import("./models/issue");
